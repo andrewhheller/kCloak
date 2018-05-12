31c8f0122d3224d6d22ac284190fd4b7a2d6dd00
Andrew Heller <andrewhheller@gmail.com>
2018-05-12 19:42:35 -0400

    consolidated JS code into index.html, removed CSS

--- kcloak.js
+++
@@ -1,9 +0,0 @@
-	<script>
-		let x = klingonCloak("info","listservenyc.com");
-
-		function klingonCloak(a,b) {
-			return a + "@" + b;
-		}
-
-		document.getElementById("email-text").innerHTML = x;
-	</script>
